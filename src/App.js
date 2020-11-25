0917616d41f913f829fa1ce2a840c23abcec578a
rqsell <68514875+rqsell@users.noreply.github.com>
2020-11-24 23:57:28 -0500

    adding api

--- src/App.js
+++ src/App.js
@@ -1,70 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { Link } from "react-router-dom";
-import Explore from './components/Explore'
-import Ono from './components/Ono'
-import Kahlo from './components/Kahlo'
-import Krater from './components/Krater'
-import Goya from './components/Goya'
-import QRCodes from "./components/QRCodes"
-import React, { useState } from 'react';
-import {BrowserRouter, Switch, Route, NavLink, useHistory } from "react-router-dom"; 
-import GetStarted from './components/GetStarted';
-
+import Explore from "./components/Explore";
+import Ono from "./components/Ono";
+import Kahlo from "./components/Kahlo";
+import Krater from "./components/Krater";
+import Goya from "./components/Goya";
+import QRCodes from "./components/QRCodes";
+import React, { useState } from "react";
+import API from "./components/API.js";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  NavLink,
+  useHistory,
+} from "react-router-dom";
+import GetStarted from "./components/GetStarted";
 
 function App() {
   const [open, setOpen] = useState(false);
   return (
-   
-  <div>
-   
-<BrowserRouter>
-<div className='head'>
-  <Link to='/'>
-            <img src= "./Ruseum.png" alt="icon" className="icon" />
-</Link>
-          
-            <nav role='navigation'>
-  <div id="menuToggle">
-            <input type="checkbox" onClick={() => setOpen(!open)}/>
+    <div>
+      <BrowserRouter>
+        <div className="head">
+          <Link to="/">
+            <img src="./Ruseum.png" alt="icon" className="icon" />
+          </Link>
 
-            <span></span>
-            <span></span>
-           <span></span>
+          <nav role="navigation">
+            <div id="menuToggle">
+              <input type="checkbox" onClick={() => setOpen(!open)} />
 
-            <ul className="hamburgerMenu" id={open ? "clickedmenu" : ""}>
-           
-          {/* <div className="x" onClick={() => setOpen(!open)}>
+              <span></span>
+              <span></span>
+              <span></span>
+
+              <ul className="hamburgerMenu" id={open ? "clickedmenu" : ""}>
+                {/* <div className="x" onClick={() => setOpen(!open)}>
             X
           </div> */}
-          <Link to="/" style={{ textDecoration: "none" }} onClick={() => setOpen(!open)}>
-            <li>Get Started</li>
-       </Link>
-       <Link to="/Explore" style={{ textDecoration: "none" }} onClick={() => setOpen(!open)}>
-            <li>Explore</li>
-   </Link>
-   <Link to="/QRCodes" style={{ textDecoration: "none" }} onClick={() => setOpen(!open)}>
-            <li>QR Codes</li>
-      </Link>
-       
-      </ul>
-        </div>
-        </nav>
-      
-      
+                <Link
+                  to="/"
+                  style={{ textDecoration: "none" }}
+                  onClick={() => setOpen(!open)}
+                >
+                  <li>Get Started</li>
+                </Link>
+                <Link
+                  to="/Explore"
+                  style={{ textDecoration: "none" }}
+                  onClick={() => setOpen(!open)}
+                >
+                  <li>Explore</li>
+                </Link>
+                <Link
+                  to="/QRCodes"
+                  style={{ textDecoration: "none" }}
+                  onClick={() => setOpen(!open)}
+                >
+                  <li>QR Codes</li>
+                </Link>
+              </ul>
             </div>
-  <Switch>
-   <Route exact path="/" render={(props) => <GetStarted {...props} />} />
-   <Route exact path="/Explore" render={(props) => <Explore {...props} />} />
-   <Route exact path="/QRCodes" render={(props) => <QRCodes {...props} />} />
-   <Route exact path="/Ono" render={(props) => <Ono {...props} />} />
-   <Route exact path="/Kahlo" render={(props) => <Kahlo {...props} />} />
-   <Route exact path="/Krater" render={(props) => <Krater {...props} />} />
-   <Route exact path="/Goya" render={(props) => <Goya {...props} />} />
-   
-  </Switch>
-  </BrowserRouter>
-  </div>
+          </nav>
+        </div>
+        <Switch>
+          <Route exact path="/" render={(props) => <GetStarted {...props} />} />
+          <Route
+            exact
+            path="/Explore"
+            render={(props) => <Explore {...props} />}
+          />
+          <Route
+            exact
+            path="/QRCodes"
+            render={(props) => <QRCodes {...props} />}
+          />
+          <Route exact path="/Ono" render={(props) => <Ono {...props} />} />
+          <Route exact path="/Kahlo" render={(props) => <Kahlo {...props} />} />
+          <Route
+            exact
+            path="/Krater"
+            render={(props) => <Krater {...props} />}
+          />
+          <Route exact path="/Goya" render={(props) => <Goya {...props} />} />
+          <Route exact path="/API" render={(props) => <API {...props} />} />
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ src/components/API.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import request from "superagent";
+import traverson from "traverson";
+import JsonHalAdapter from "traverson-hal";
+
+function API(props) {
+  const [frida, setFrida] = useState({});
+
+  //   "token":"eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsInN1YmplY3RfYXBwbGljYXRpb24iOiI1ZjQ2OGU4M2Y2MmY1ZTAwMTE1NmVkZjMiLCJleHAiOjE2MDY4NzcyMjMsImlhdCI6MTYwNjI3MjQyMywiYXVkIjoiNWY0NjhlODNmNjJmNWUwMDExNTZlZGYzIiwiaXNzIjoiR3Jhdml0eSIsImp0aSI6IjVmYmRjNWE3NWUzZGI0NWU2NDQ2OGMwOCJ9.H1ofNDJKGy_BMuPwusge4PTgVuj_QayCGImaoWsVkws","expires_at":"2020-12-02T02:47:03+00:00"
+
+  let xappToken =
+    "eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsInN1YmplY3RfYXBwbGljYXRpb24iOiI1ZjQ2OGU4M2Y2MmY1ZTAwMTE1NmVkZjMiLCJleHAiOjE2MDY4NzcyMjMsImlhdCI6MTYwNjI3MjQyMywiYXVkIjoiNWY0NjhlODNmNjJmNWUwMDExNTZlZGYzIiwiaXNzIjoiR3Jhdml0eSIsImp0aSI6IjVmYmRjNWE3NWUzZGI0NWU2NDQ2OGMwOCJ9.H1ofNDJKGy_BMuPwusge4PTgVuj_QayCGImaoWsVkws";
+  useEffect(() => {
+    traverson.registerMediaType(JsonHalAdapter.mediaType, JsonHalAdapter);
+    let api = traverson.from("https://api.artsy.net/api").jsonHal();
+
+    api
+      .newRequest()
+      .follow("artist")
+      .withRequestOptions({
+        headers: {
+          "X-Xapp-Token": xappToken,
+          Accept: "application/vnd.artsy-v2+json",
+        },
+      })
+      .withTemplateParameters({ id: "frida-kahlo" })
+      .getResource(function (error, fridaKahlo) {
+        console.log(fridaKahlo);
+        setFrida(fridaKahlo);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <h1>{frida?.name}</h1>
+        <img src={frida?._links.thumbnail.href} />
+        <ul>
+          <li> Born: {frida.birthdate} </li>
+          <li>Died: {frida.deathday}</li>
+          <li> Nationality: {frida.nationality}</li>
+          <li> Hometown: {frida.hometown} </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default API;

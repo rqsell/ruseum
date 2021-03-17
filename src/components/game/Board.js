1179708b7119615c7fd91b85f6f4d8b0b143df50
rqsell <68514875+rqsell@users.noreply.github.com>
2021-03-17 17:06:12 -0400

    adding start and shuffle features

--- src/components/game/Board.js
+++ src/components/game/Board.js
@@ -6,8 +6,28 @@ import { canSwap, shuffle, swap, isSolved } from "./helpers"
 function Board({ imgUrl }) {
   const [tiles, setTiles] = useState([...Array(TILE_COUNT).keys()]);
   const [isStarted, setIsStarted] = useState(false);
+  const [isSolved, setIsSolved]= useState(false);
 
-
+const shuffleTiles= () => {
+    const shuffeledTiles= shuffle(tiles)
+    setTiles(shuffleTiles)
+}
+const swapTiles= (tileIndex) =>{ 
+if(canSwap(tileIndex, tile.length-1 )){
+    const swappedTiles=  swap(tiles, tileIndex, tiles.length -1)
+    setTiles(swappedTiles)
+}
+}
+const handleTileClick= (index) =>{
+     swapTiles(index)
+}
+const handleShuffleClick = () => {
+    shuffleTiles()
+}
+const handleStartClick = () => {
+    shuffleTiles()
+    setIsStarted(true)
+}
   const pieceWidth = Math.round(BOARD_SIZE / GRID_SIZE);
   const pieceHeight = Math.round(BOARD_SIZE / GRID_SIZE);
   const style = {
@@ -17,7 +37,7 @@ function Board({ imgUrl }) {
 
 
   return (
-    <>
+    <>zz
       <ul style={style} className="board">
         {tiles.map((tile, index) => (
           <Tile
@@ -31,7 +51,9 @@ function Board({ imgUrl }) {
           />
         ))}
       </ul>
+     {!isStarted ? (<button onClick={() => handleStartClick}> Start Game </button>): (    <button onClick={() => handleShuffleClick}> Restart Game</button> )}
     
+  
     </>
   );
 }
